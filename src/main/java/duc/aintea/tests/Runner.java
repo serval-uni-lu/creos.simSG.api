149eebcb1eeaba52ecd9740c87a629064809370e
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-01-24 15:44:29 +0100

    add a runner for manual test

---
+++ src/main/java/duc/aintea/tests/Runner.java
@@ -0,0 +1,10 @@
+package duc.aintea.tests;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+//        Fuse f1 = new Fuse("f1");
+//        f1.status = State.CLOSED;
+    }
+}
