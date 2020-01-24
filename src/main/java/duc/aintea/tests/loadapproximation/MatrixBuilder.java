385e832df3694e5fa0fc426d7f90438c043243ad
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-01-24 15:47:13 +0100

    modify matrix builder

--- src/main/java/duc/aintea/tests/loadapproximation/MatrixBuilder.java
+++ src/main/java/duc/aintea/tests/loadapproximation/MatrixBuilder.java
@@ -1,42 +1,30 @@
 package duc.aintea.tests.loadapproximation;
 
-import duc.aintea.tests.sg.Fuse;
-import duc.aintea.tests.sg.Grid;
+import duc.aintea.tests.sg.Substation;
 import org.ejml.data.DenseMatrix64F;
-
-import java.util.List;
 
 public class MatrixBuilder {
 
-    public double[] build(Grid grid) {
+    public double[] build(Substation substation) {
         var cableEq = new DenseMatrix64F(0);
         var deadEndsEq = new DenseMatrix64F(0);
 
+        var neighbors = substation.getReachableEntities();
+        cableEq.reshape(neighbors.size(), neighbors.size() * 2);
 
-        var substation = grid.getSubstations().get(0);
+        for (int i = 0; i < neighbors.size(); i++) {
+            cableEq.add(i, 0, 1);
+            cableEq.add(i, 1, 1);
 
-        List<Fuse> fuses = substation.getFuses();
-        for (int i = 0; i < fuses.size(); i++) {
-            var f = fuses.get(i);
-            var opposite = f.getOpposite();
-            if (f.getOpposite() != null) {
-                cableEq.reshape(cableEq.numRows + 1, cableEq.numRows + 2, true);
-                deadEndsEq.reshape(deadEndsEq.numRows, cableEq.numCols, true);
-                cableEq.add(i, 0, 1);
-                cableEq.add(i, 1,1);
-            }
-
-            if(opposite.isDeadEnd()) {
-               deadEndsEq.reshape(deadEndsEq.numRows + 1, deadEndsEq.numCols, true);
-               deadEndsEq.set(deadEndsEq.numRows - 1, i+1, 1);
+            if(neighbors.get(i).isDeadEnd()) {
+                deadEndsEq.reshape(deadEndsEq.numRows + 1, cableEq.numCols, true);
+                deadEndsEq.add(deadEndsEq.numRows - 1, 1, 1);
             }
         }
 
         double[] resData = new double[cableEq.data.length + deadEndsEq.data.length];
         System.arraycopy(cableEq.data, 0, resData, 0, cableEq.data.length);
         System.arraycopy(deadEndsEq.data, 0, resData, cableEq.data.length ,deadEndsEq.data.length);
-
-
 
         return resData;
     }
