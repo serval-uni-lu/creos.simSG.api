bae344a3170d04e6d28434688d56a897d6c43728
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-01-27 08:53:30 +0100

    runner now run Scenario 1

--- src/main/java/duc/aintea/tests/Runner.java
+++ src/main/java/duc/aintea/tests/Runner.java
@@ -1,10 +1,14 @@
 package duc.aintea.tests;
 
+import duc.aintea.tests.loadapproximation.Scenario1;
+
+import java.util.Arrays;
+
 public class Runner {
 
 
     public static void main(String[] args) {
-//        Fuse f1 = new Fuse("f1");
-//        f1.status = State.CLOSED;
+        Scenario1 sc1 = new Scenario1(10);
+        System.out.println(Arrays.toString(sc1.computeLoad()));
     }
 }
