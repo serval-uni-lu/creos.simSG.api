d3acfb627bfb475c9a738a33cc7cf0af4796454f
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-01-31 12:13:32 +0100

    add a non-regression test

--- src/test/java/duc/aintea/tests/loadapproximation/DeadEndTest.java
+++ src/test/java/duc/aintea/tests/loadapproximation/DeadEndTest.java
@@ -138,4 +138,46 @@ public class DeadEndTest {
         assertTrue(c2.isDeadEnd());
     }
 
+    /*
+                                          |-[f3]----(cbl2)----[f5]-cab2
+        subs-[f1]----(cbl1)----[f2]-cab1-<
+                                          |-[f4]----(cbl3)----[f6]-cab3
+     */
+    @Test
+    public void cabinetScenario() {
+        var subs = new Substation("substation");
+        var cab1 = new Cabinet("cabinet1");
+        var cab2 = new Cabinet("cabinet1");
+        var cab3 = new Cabinet("cabinet1");
+
+        var f1 = new Fuse("fuse_subs");
+        var f2 = new Fuse("fuse1_cabinet_1");
+        var f3 = new Fuse("fuse2_cabinet_1");
+        var f4 = new Fuse("fuse3_cabinet_1");
+        var f5 = new Fuse("fuse_cabinet2");
+        var f6 = new Fuse("fuse_cabinet3");
+
+        var cbl1 = new Cable();
+        var cbl2 = new Cable();
+        var cbl3 = new Cable();
+
+        subs.addFuses(f1);
+        cab1.addFuses(f2, f3, f4);
+        cab2.addFuses(f5);
+        cab3.addFuses(f6);
+
+        cbl1.setFuses(f1, f2);
+        cbl2.setFuses(f3, f5);
+        cbl3.setFuses(f4, f6);
+
+        f5.openFuse();
+        f6.openFuse();
+
+        assertFalse(subs.isDeadEnd());
+        assertFalse(cab1.isDeadEnd());
+        assertTrue(cab2.isDeadEnd());
+        assertTrue(cab3.isDeadEnd());
+
+    }
+
 }
