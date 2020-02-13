c70e85a5727e913a63b0b97db5436862d1460f34
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-02-13 17:15:02 +0100

    re-add WS

--- website/src/main.ts
+++ website/src/main.ts
@@ -11,3 +11,38 @@ let app = new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+
+
+
+let RQ_SAYS_ACT = "actuator-hello/"
+
+
+let wsURL = 'ws://localhost:8585/ws';
+var ws = new WebSocket(wsURL);
+ws.onopen = function () {
+  console.debug("WebSocket connected to " + wsURL)
+};
+
+ws.onmessage = function(event) {
+  var message = event.data;
+  if(message.startsWith(RQ_SAYS_ACT)) {
+    message = message.substr(RQ_SAYS_ACT.length);
+    message = JSON.parse(message);
+
+    console.debug("[" + wsURL + "] Data received: ");
+    console.debug(message);
+
+    app.$store.commit('addActuator', message);
+
+  }
+  
+};
+
+ws.onclose = function(event) {
+  console.debug("Server has closed the connection");
+  console.log(event);
+};
+
+
