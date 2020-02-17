3fbffb61064a483da8239c5658f8ddaae5f89d81
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-02-17 18:59:02 +0100

    improve layout fuse status change

--- website/src/components/scenarioView/attrViews/InspFuse.vue
+++ website/src/components/scenarioView/attrViews/InspFuse.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
     div
-        | State:
-        input(type="checkbox" id="fuseStatus" v-model.boolean="fuses[fuseId].isClosed")
-        label(for="fuseStatus") (is closed?)
+        | State: 
+        .checkbox 
+            label(class="switch")
+                input(type="checkbox" id="fuseStatus" v-model.boolean="fuses[fuseId].isClosed")
+                span(class="slider")
+        span(v-if="fuses[fuseId].isClosed" class="stateInfo") (Closed)
+        span(v-else class="stateInfo") (Open)
         br
         | Load:
         | {{load}}
+        br
+        
+        
 
 </template>
 
@@ -29,9 +36,79 @@ export default {
 }
 </script>
 
-<style scoped>
-input {
+<style lang="scss" scoped>
+
+.stateInfo {
+    display: inline;
+    position: relative;
+    left: 40px;
+    font-size: 0.9em;
+    font-style: italic;
+}
+
+.checkbox {
+    display: inline;
+    position: relative;
+    top: 6px;
+    left: 10px;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 16px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 12px;
+  width: 12px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: $link-color;
+}
+
+input:focus + .slider {
+    left: 50px;
+    box-shadow: 0 0 1px $link-color;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(14px);
+  -ms-transform: translateX(14px);
+  transform: translateX(14px);
+}
+
+
+
+/* input {
     width: 15%;
     margin-left: 20px; 
-}
+} */
 </style>
