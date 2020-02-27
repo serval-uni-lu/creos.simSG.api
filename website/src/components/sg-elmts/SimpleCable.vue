352f5d6a6a7fadd2faed2a317d94cc5feb85a372
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-02-27 12:18:43 +0100

    improve overlayer box for simple cable

--- website/src/components/sg-elmts/SimpleCable.vue
+++ website/src/components/sg-elmts/SimpleCable.vue
@@ -12,17 +12,17 @@
             <circle :cx=line1.x1 :cy=circle.y r="5"/>
             <circle :cx=circle.x :cy=circle.y r="7" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
         </g>
-        <g :transform=gPosition>
-            <rect x="0" y="0" rx="8" ry="8" width="54" height="45" fill="white" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+        <g :transform=gPosition :visibility=showOLInfo class="infoBox">
+            <rect x="0" y="0" rx="8" ry="8" width="75" :height="heightOLBox()" fill="white"  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
             <text transform="translate(5 5)" fill="black">
-                <tspan id="AHAHAHAHAHA" font-family="Helvetica Neue" font-size="8" font-weight="700" fill="black" x="12" y="5">Load</tspan>
-                <tspan font-family="Helvetica Neue" font-size="8" font-weight="400" fill="black" x="0" y="15">- 10A [10%]</tspan>
-                <tspan font-family="Helvetica Neue" font-size="8" font-weight="400" fill="black" x="0" y="25">- 20 A [80%]</tspan>
-                <tspan font-family="Helvetica Neue" font-size="8" font-weight="400" fill="black" x="0" y="35">- 0 A [10%]</tspan>
+                <tspan font-family="Helvetica Neue" font-size="8" font-weight="700" x="26" y="5">Load</tspan>
+                <tspan v-for="ul in uloads()" :key="ul.id" font-family="Helvetica Neue" font-size="8" font-weight="400" x="0" :y="ul.y">- {{ul.value}} A [{{ul.confidence}}%]</tspan>
             </text>
         </g>    
     </g>
 </template>
+
+// v-bind:class="{active: infoOverLayerVis}"
 
 <script>
 import { mapState } from 'vuex'
@@ -35,6 +35,9 @@ export default {
         circle: Object
     },
     computed: {
+        showOLInfo: function() {
+            return (this.infoOverLayerVis)? "visible": "hidden";
+        },
         gPosition: function() {
             return "translate(" + (this.xLoadInfo) + " " + this.yLoadInfo + ")";
         },
@@ -50,12 +53,36 @@ export default {
         ...mapState({
             selectedElmt: state => state.selectedElmt,
             loads: state => state.cableLoads,
+            uCableLoads: state => state.uCableLoads,
+            infoOverLayerVis: state => state.infoOverLayerVis
         })
     },
     methods: {
+        heightOLBox: function() {
+            return 15 + this.uloads().length * 10;
+        },
         load: function() {
             var p_load = this.loads[this.id];
             return (p_load === undefined || p_load === -1)? "To be computed..." : p_load.toFixed(2) + " A";
+        },
+        uloads: function() {
+            var uloads = this.uCableLoads[this.id];
+            if(uloads === undefined || uloads.length === 0) {
+                return [{id: 0, value: "??", confidence: "??", y: 15}]
+            }
+
+            var result = []
+            for(var ul=0; ul<uloads.length; ul++) {
+                var curr = {
+                    id: ul,
+                    value: uloads[ul].value.toFixed(2),
+                    confidence: (uloads[ul].confidence * 100).toFixed(2),
+                    y: 5 + 10*(ul+1)
+                };
+                result.push(curr);
+            }
+            
+            return result;
         },
         showInspector: function() {
             let info = {
@@ -76,4 +103,14 @@ export default {
     user-select: none;
 }
 
+
+.infoBox {
+    rect {
+        stroke: $color-selection,
+    }
+
+    tspan {
+        fill: $color-selection
+    }
+}
 </style>
