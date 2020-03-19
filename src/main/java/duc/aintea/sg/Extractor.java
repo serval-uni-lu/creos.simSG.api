05011081c2bba6ec79392948d950d346d6040b3d
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-03-19 10:50:23 +0100

    add fuse extractor

--- src/main/java/duc/aintea/sg/Extractor.java
+++ src/main/java/duc/aintea/sg/Extractor.java
@@ -8,6 +8,34 @@ import java.util.Stack;
 public class Extractor {
     private Extractor(){}
 
+    public static List<Fuse> extractFuses(Substation substation) {
+        var res = new ArrayList<Fuse>();
+
+        var waiting = new Stack<Fuse>();
+        var visited = new HashSet<Fuse>();
+
+        waiting.add(substation.getFuses().get(0));
+
+        while (!waiting.isEmpty()) {
+            var current = waiting.pop();
+            visited.add(current);
+            res.add(current);
+
+            var opp = current.getOpposite();
+            visited.add(opp);
+            res.add(opp);
+
+            var ownerOpp = opp.getOwner();
+            for(var f: ownerOpp.getFuses()) {
+                if(!visited.contains(f)) {
+                    waiting.add(f);
+                }
+            }
+        }
+
+        return res;
+    }
+
     public static List<Cable> extractCables(Substation substation) {
         var res = new ArrayList<Cable>();
 
--- src/test/java/duc/aintea/loadapproximation/test/sg/ExtractorTest.java
+++ src/test/java/duc/aintea/loadapproximation/test/sg/ExtractorTest.java
@@ -2,6 +2,7 @@ package duc.aintea.loadapproximation.test.sg;
 
 import duc.aintea.sg.Cable;
 import duc.aintea.sg.Extractor;
+import duc.aintea.sg.Fuse;
 import duc.aintea.sg.Substation;
 import duc.aintea.sg.scenarios.*;
 import org.junit.jupiter.api.Assertions;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class ExtractorTest {
 
-    private void genTest(Cable[] expected, List<Cable> actual) {
+    private void genExtCableTest(Cable[] expected, List<Cable> actual) {
         var uniqueSet = new HashSet<>(actual);
         Assertions.assertEquals(expected.length, uniqueSet.size());
         for(Cable c: expected) {
@@ -20,44 +21,92 @@ public class ExtractorTest {
         }
     }
 
+    private void genExtFusesTest(Fuse[] expected, List<Fuse> actual) {
+        var uniqueSet = new HashSet<>(actual);
+        Assertions.assertEquals(expected.length, uniqueSet.size());
+        for(Fuse f: expected) {
+            Assertions.assertTrue(uniqueSet.contains(f));
+        }
+    }
+
     @Test
-    public void testSingleCable() {
+    public void testExtCableSingleCable() {
         Substation substation = SingleCableBuilder.build();
         Cable[] expected = SingleCableBuilder.extractCables(substation);
         List<Cable> actual = Extractor.extractCables(substation);
-       genTest(expected, actual);
+        genExtCableTest(expected, actual);
     }
 
     @Test
-    public void testCab() {
+    public void testExtCableCab() {
         Substation substation = CabinetBuilder.build();
         Cable[] expected = CabinetBuilder.extractCables(substation);
         List<Cable> actual = Extractor.extractCables(substation);
-        genTest(expected, actual);
+        genExtCableTest(expected, actual);
     }
 
     @Test
-    public void testParaTrans() {
+    public void testExtCableParaTrans() {
         Substation substation = ParaTransformerBuilder.build();
         Cable[] expected = ParaTransformerBuilder.extractCables(substation);
         List<Cable> actual = Extractor.extractCables(substation);
-        genTest(expected, actual);
+        genExtCableTest(expected, actual);
     }
 
     @Test
-    public void testParaCab() {
+    public void testExtCableParaCab() {
         Substation substation = ParaCabinetBuilder.build();
         Cable[] expected = ParaCabinetBuilder.extractCables(substation);
         List<Cable> actual = Extractor.extractCables(substation);
-        genTest(expected, actual);
+        genExtCableTest(expected, actual);
     }
 
     @Test
-    public void testIndPara() {
+    public void testExtCableIndPara() {
         Substation substation = IndirectParaBuilder.build();
         Cable[] expected = IndirectParaBuilder.extractCables(substation);
         List<Cable> actual = Extractor.extractCables(substation);
-        genTest(expected, actual);
+        genExtCableTest(expected, actual);
+    }
+
+    @Test
+    public void testExtFusesSingleCable() {
+        Substation substation = SingleCableBuilder.build();
+        Fuse[] expected = SingleCableBuilder.extractFuses(substation);
+        List<Fuse> actual = Extractor.extractFuses(substation);
+        genExtFusesTest(expected, actual);
+    }
+
+    @Test
+    public void testExtFuseCab() {
+        Substation substation = CabinetBuilder.build();
+        Fuse[] expected = CabinetBuilder.extractFuses(substation);
+        List<Fuse> actual = Extractor.extractFuses(substation);
+        genExtFusesTest(expected, actual);
+    }
+
+    @Test
+    public void testExtFuseParaTrans() {
+        Substation substation = ParaTransformerBuilder.build();
+        Fuse[] expected = ParaTransformerBuilder.extractFuses(substation);
+        List<Fuse> actual = Extractor.extractFuses(substation);
+        genExtFusesTest(expected, actual);
+    }
+
+    @Test
+    public void testExtFuseParaCab() {
+        Substation substation = ParaCabinetBuilder.build();
+        Fuse[] expected = ParaCabinetBuilder.extractFuses(substation);
+        List<Fuse> actual = Extractor.extractFuses(substation);
+        genExtFusesTest(expected, actual);
+    }
+
+    @Test
+    public void testExtFuseIndPara() {
+        Substation substation = IndirectParaBuilder.build();
+        Fuse[] expected = IndirectParaBuilder.extractFuses(substation);
+        List<Fuse> actual = Extractor.extractFuses(substation);
+        genExtFusesTest(expected, actual);
     }
 
 }
