a05c99ba8d4c52c6b78c2ed3efce64e75fb58707
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-03-24 11:43:20 +0100

    add helper for JsonImporter tests

---
+++ src/test/java/duc/aintea/loadapproximation/test/JsonImporterHelper.java
@@ -0,0 +1,31 @@
+package duc.aintea.loadapproximation.test;
+
+import duc.aintea.loadapproximation.test.matrixBuilder.certain.SuccessiveParaTest;
+import duc.aintea.sg.Substation;
+import duc.aintea.sg.importer.JsonImporter;
+import duc.aintea.sg.importer.ValidationException;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JsonImporterHelper {
+    private JsonImporterHelper() {}
+
+    public static Substation importSubs(String fileName) throws ValidationException {
+        final String jsonPath = SuccessiveParaTest.class
+                .getClassLoader()
+                .getResource("validJson/sg/" + fileName)
+                .getPath();
+        final var file = new File(jsonPath);
+
+        Optional<List<Substation>> optionalSubstations = JsonImporter.from(file);
+        assertTrue(optionalSubstations.isPresent());
+        List<Substation> substations = optionalSubstations.get();
+
+        return substations.get(0);
+    }
+
+}
