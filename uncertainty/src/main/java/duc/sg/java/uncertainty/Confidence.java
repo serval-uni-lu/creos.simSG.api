bcda6322db7b49f1e33246ef5b8839d44ac54670
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-04-02 12:52:25 +0200

    add precision to the confidence probability

--- uncertainty/src/main/java/duc/sg/java/uncertainty/Confidence.java
+++ uncertainty/src/main/java/duc/sg/java/uncertainty/Confidence.java
@@ -4,19 +4,22 @@ public class Confidence {
     protected double probability;
     public static final int MAX_PROBABILITY = 1;
     public static final int MIN_PROBABILITY = 0;
+    public static final double PRECISION = 0.0009;
 
     public Confidence(double probability) {
         setProbability(probability);
     }
 
     static void checkProb(double probability) {
-        if (probability < MIN_PROBABILITY || probability > MAX_PROBABILITY) {
+        if (probability < MIN_PROBABILITY - PRECISION || probability > MAX_PROBABILITY + PRECISION) {
             throw new IllegalArgumentException("Confidence should be between 0 and 1.");
         }
     }
 
     public void setProbability(double probability) {
         checkProb(probability);
+        if(Math.abs(probability - MIN_PROBABILITY) <= PRECISION) probability = MIN_PROBABILITY;
+        else if(Math.abs(probability - MAX_PROBABILITY) <= PRECISION) probability = MAX_PROBABILITY;
         this.probability = probability;
     }
 
--- uncertainty/src/test/java/duc/sg/java/uncertainty/math/test/ConfidenceTest.java
+++ uncertainty/src/test/java/duc/sg/java/uncertainty/math/test/ConfidenceTest.java
@@ -14,7 +14,27 @@ public class ConfidenceTest {
     @MethodSource("duc.sg.java.uncertainty.math.test.ConfidenceDataGenerator#generateWrongProb")
     public void testWringProb(double val) {
         var conf = new Confidence(1);
-        assertThrows(IllegalArgumentException.class, () -> {conf.setProbability(val);});
+        assertThrows(IllegalArgumentException.class, () -> conf.setProbability(val));
+    }
+
+    @Test
+    public void testNearMax() {
+        var conf = new Confidence(1);
+        assertDoesNotThrow(() -> conf.setProbability(1.0001));
+        assertEquals(Confidence.MAX_PROBABILITY, conf.getProbability());
+        assertDoesNotThrow(() -> conf.setProbability(1.0009));
+        assertEquals(Confidence.MAX_PROBABILITY, conf.getProbability());
+        assertThrows(IllegalArgumentException.class, () -> conf.setProbability(1.001));
+    }
+
+    @Test
+    public void testNearMin() {
+        var conf = new Confidence(1);
+        assertDoesNotThrow(() -> conf.setProbability(-0.0001));
+        assertEquals(Confidence.MIN_PROBABILITY, conf.getProbability());
+        assertDoesNotThrow(() -> conf.setProbability(-0.0009));
+        assertEquals(Confidence.MIN_PROBABILITY, conf.getProbability());
+        assertThrows(IllegalArgumentException.class, () -> conf.setProbability(-0.001));
     }
 
     @Test
