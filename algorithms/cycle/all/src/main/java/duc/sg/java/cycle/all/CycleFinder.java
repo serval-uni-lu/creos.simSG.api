b6707ac42436a751b6c310586d02d1b9bc5a3a5c
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-07-22 16:01:07 +0200

    manage inner cirlce + remove enable preview to allow stream debugging in Intellij

--- algorithms/cycle/all/src/main/java/duc/sg/java/cycle/all/CycleFinder.java
+++ algorithms/cycle/all/src/main/java/duc/sg/java/cycle/all/CycleFinder.java
@@ -2,6 +2,9 @@ package duc.sg.java.cycle.all;
 
 import duc.sg.java.model.Substation;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CycleFinder {
     static CycleFinder getDefault() {
         return new CycleFinderImpl();
@@ -12,6 +15,16 @@ public interface CycleFinder {
      *
      * @param substation
      */
-    void getAndSaveCycles(Substation substation);
+    void findAndSaveCycles(Substation substation);
+
+    default List<Cycle> getCycles(Substation substation) {
+        Optional<Object> optCircles = substation.getGrid().retrieve(CycleUtils.getKey(substation));
+        if(optCircles.isEmpty()) {
+            findAndSaveCycles(substation);
+            optCircles = substation.getGrid().retrieve(CycleUtils.getKey(substation));
+        }
+
+        return (List<Cycle>) optCircles.get();
+    }
 
 }
