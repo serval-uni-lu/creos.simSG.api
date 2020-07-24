e0980adc17ac49b0b879a410e417ba562aae0cdc
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-07-24 10:47:51 +0200

    clean substation code

--- model/src/main/java/duc/sg/java/model/Substation.java
+++ model/src/main/java/duc/sg/java/model/Substation.java
@@ -2,7 +2,6 @@ package duc.sg.java.model;
 
 
 public class Substation extends Entity {
-//    private Collection<Fuse> allFuses;
     private SmartGrid grid;
 
     public Substation(String name) {
@@ -16,10 +15,6 @@ public class Substation extends Entity {
     public SmartGrid getGrid() {
         return grid;
     }
-
-//    public Collection<Fuse> getAllFuses() {
-//        return allFuses;
-//    }
 
     @Override
     public boolean isDeadEnd() {
@@ -35,60 +30,4 @@ public class Substation extends Entity {
     public boolean isAlwaysDeadEnd() {
         return false;
     }
-
-//    public <T> void extract(Collection<T> collection, Collector<T> collector) {
-//        var waiting = new Stack<Fuse>();
-//        var inWaitingList = new HashSet<Fuse>(); //real optimization ??
-//        var visited = new HashSet<Fuse>();
-//
-//        if(!this.getFuses().isEmpty()) {
-//            waiting.add(this.getFuses().get(0));
-//        }
-//
-//        while (!waiting.isEmpty()) {
-//            var current = waiting.pop();
-//            visited.add(current);
-//
-//            collector.collect(collection, current);
-//
-//            var ownerOpp = current.getOpposite().getOwner();
-//            for(var f: ownerOpp.getFuses()) {
-//                if(!visited.contains(f) && !inWaitingList.contains(f)) {
-//                    waiting.add(f);
-//                    inWaitingList.add(f);
-//                }
-//            }
-//        }
-//    }
-
-//    public void updateAllFuses() {
-//        allFuses = extractFuses();
-//    }
-
-//    public Collection<Fuse> extractFuses() {
-//        final var res = new HashSet<Fuse>();
-//        extract(res, (collection, currentFuse) -> {
-//            if(!collection.contains(currentFuse)) {
-//                collection.add(currentFuse);
-//            }
-//        });
-//        return Collections.unmodifiableCollection(res);
-//    }
-
-//    public Collection<Cable> extractCables() {
-//        final var res = new HashSet<Cable>();
-//        extract(res, (collection, currentFuse) -> {
-//            Cable cable = currentFuse.getCable();
-//            if(!collection.contains(cable)) {
-//                collection.add(cable);
-//            }
-//        });
-//        return Collections.unmodifiableCollection(res);
-//    }
-
-
-
-//    public interface Collector<T> {
-//        void collect(Collection<T> collection, Fuse currentFuse);
-//    }
 }
